2e4fc13d8b7f7cd9a2509aa05a606ba14a5122d7
welferdinand <welferdinand@gmail.com>
2024-04-24 16:23:35 +0700

    second commit

---
+++ data.js
@@ -0,0 +1,10 @@
+let komikList = [{
+    id: 1,
+    judul: "Naruto",
+    imgCover: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_s4xo19i857bg4UAJZsXYCyhKOYwHmbIKvXABYqTG2g&s",
+    rating: 8,
+    genres: "Action, Adventure, Fantasy",
+    aired: "Oct 3, 2002 to Feb 8, 2007"
+},
+
+]
