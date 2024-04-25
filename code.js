df627b97b64d65c68fd889cdd30929ef73b7d10b
zainalalmasyhur <zainalalmasyhur30@gmail.com>
2024-04-26 00:52:27 +0700

    jenal

--- code.js
+++ code.js
@@ -6,7 +6,7 @@ function render(array) {
   listKomik.innerHTML = "";
 
   for (let i = 0; i < array.length; i++) {
-    let { id, judul, imgCover, rating, genres, aired } = array[i];
+    let { id, judul, imgCover, rating, genres, aired, wiki } = array[i];
 
     listKomik.innerHTML += `<div class="card rounded" style="width: 18rem;">
     <div class="img rounded">
@@ -31,10 +31,10 @@ function render(array) {
     </svg>  </a>
     </div>
       <div class="text-center ">
-      <a href="#" class="btn btn-outline-warning"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-      <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-      <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
-      </svg></a>
+      <a href="${wiki}" class="btn btn-outline-warning"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+      <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/>
+      <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
+    </svg></a>
       </div>
       <div class="text-center ">
       <a href="#" class="btn btn-outline-danger delete" data-id="${id}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
@@ -177,4 +177,73 @@ function simpanEdit() {
   arrayYangSudahBerubah = [...newArray];
 
   render(newArray);
+}
+
+function createMovie() {
+  
+  let Mjudul = document.getElementById("input-title");
+  let categorySelector = document.getElementById("input-category");
+  let yearSelector = document.getElementById("input-year");
+  let castSelector = document.getElementById("input-cast");
+  let descSelector = document.getElementById("input-desc");
+  let trailerSelector = document.getElementById("input-trailer");
+  let imageSelector = document.getElementById("input-link-img");
+
+  
+  let titleValue = Mjudul.value;
+  let categoryValue = categorySelector.value;
+  let yearValue = yearSelector.value;
+  let castValue = castSelector.value;
+  let descValue = descSelector.value;
+  let trailerValue = trailerSelector.value;
+  let imageValue = imageSelector.value;
+
+  if (
+    !titleValue ||
+    !categoryValue ||
+    !yearValue ||
+    !castValue ||
+    !descValue ||
+    !trailerValue ||
+    !imageValue
+  ) {
+    alert("Kosong");
+  } else {
+    let tempObj = {
+      id: datas[datas.length - 1].id + 1,
+      name: titleValue,
+      category: categoryValue,
+      year: yearValue,
+      actors: castValue,
+      description: descValue,
+      link: trailerValue,
+      img: imageValue,
+    };
+
+    if (newDatas === null) {
+      datas.push(tempObj);
+      listMovies(datas);
+    } else {
+      newDatas.push(tempObj);
+      listMovies(newDatas);
+    }
+  }
+}
+
+function findManga() {
+  let result = [];
+
+  let keywordSelector = document.getElementById("isiges").value;
+  if (arrayYangSudahBerubah === null) {
+    result = komikList.filter((el) =>
+      el.judul.toLowerCase().includes(keywordSelector.toLowerCase())
+    );
+  }
+  else {
+    result = arrayYangSudahBerubah.filter((el) =>
+      el.judul.toLowerCase().includes(keywordSelector.toLowerCase())
+    );
+  }
+  //console.log(result);
+  render(result);
 }
--- data.js
+++ data.js
@@ -4,7 +4,8 @@ let komikList = [{
     imgCover: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_s4xo19i857bg4UAJZsXYCyhKOYwHmbIKvXABYqTG2g&s",
     rating: 8,
     genres: "Action, Adventure, Fantasy",
-    aired: "Oct 3, 2002 to Feb 8, 2007"
+    aired: "Oct 3, 2002 to Feb 8, 2007",
+    wiki: "https://en.wikipedia.org/wiki/Naruto"
 },
 {
     id: 2,
@@ -12,7 +13,9 @@ let komikList = [{
     imgCover: "https://cdn.myanimelist.net/images/anime/1208/94745.jpg",
     rating: 9.1,
     genres: "Action, Adventure, Drama, Fantasy",
-    aired: "Apr 5, 2009 to Jul 4, 2010"
+    aired: "Apr 5, 2009 to Jul 4, 2010",
+    wiki: "https://en.wikipedia.org/wiki/Fullmetal_Alchemist"
+
 },
 {
     id: 3,
@@ -20,7 +23,8 @@ let komikList = [{
     imgCover: "https://cdn.myanimelist.net/images/anime/10/47347.jpg",
     rating: 8.54,
     genres: "Action, Award Winning, Drama, Suspense",
-    aired: "Apr 7, 2013 to Sep 29, 2013"
+    aired: "Apr 7, 2013 to Sep 29, 2013",
+    wiki: "https://en.wikipedia.org/wiki/Attack_on_Titan"
 },
 {
     id: 4,
@@ -28,7 +32,8 @@ let komikList = [{
     imgCover: "https://cdn.myanimelist.net/images/anime/1079/138100.jpg",
     rating: 8.62,
     genres: "Supernatural, Suspense",
-    aired: "Oct 4, 2006 to Jun 27, 2007"
+    aired: "Oct 4, 2006 to Jun 27, 2007",
+    wiki: "https://en.wikipedia.org/wiki/Death_Note"
 },
 {
     id: 5,
@@ -36,7 +41,8 @@ let komikList = [{
     imgCover: "https://cdn.myanimelist.net/images/anime/1244/138851.jpg",
     rating: 8.72,
     genres: "Action, Adventure, Fantasy",
-    aired: "Oct 20, 1999 to ?"
+    aired: "Oct 20, 1999 to ?",
+    wiki: "https://en.wikipedia.org/wiki/One_Piece"
 },
 {
     id: 6,
@@ -44,7 +50,8 @@ let komikList = [{
     imgCover: "https://cdn.myanimelist.net/images/anime/1337/99013.jpg",
     rating: 9.04,
     genres: "Action, Adventure, Fantasy",
-    aired: "Oct 2, 2011 to Sep 24, 2014"
+    aired: "Oct 2, 2011 to Sep 24, 2014",
+    wiki: "https://en.wikipedia.org/wiki/Hunter_x_Hunter"
 },
 {
     id: 7,
@@ -52,7 +59,8 @@ let komikList = [{
     imgCover: "https://cdn.myanimelist.net/images/anime/1286/99889.jpg",
     rating: 8.47,
     genres: "Action, Award Winning, Fantasy",
-    aired: "Apr 6, 2019 to Sep 28, 2019"
+    aired: "Apr 6, 2019 to Sep 28, 2019",
+    wiki: "https://en.wikipedia.org/wiki/Demon_Slayer:_Kimetsu_no_Yaiba"
 },
 {
     id: 8,
@@ -60,7 +68,8 @@ let komikList = [{
     imgCover: "https://cdn.myanimelist.net/images/anime/10/78745.jpg",
     rating: 7.86,
     genres: "Action",
-    aired: "Apr 3, 2016 to Jun 26, 2016"
+    aired: "Apr 3, 2016 to Jun 26, 2016",
+    wiki: "https://en.wikipedia.org/wiki/My_Hero_Academia"
 },
 {
     id: 9,
@@ -68,7 +77,8 @@ let komikList = [{
     imgCover: "https://cdn.myanimelist.net/images/anime/10/79352.jpg",
     rating: 6.32,
     genres: "Action, Adventure, Drama, Fantasy, Horror",
-    aired: "Jul 1, 2016 to Sep 16, 2016"
+    aired: "Jul 1, 2016 to Sep 16, 2016",
+    wiki: "hhttps://en.wikipedia.org/wiki/Berserk_(manga)"
 },
 {
     id: 10,
@@ -76,7 +86,8 @@ let komikList = [{
     imgCover: "https://cdn.myanimelist.net/images/anime/1500/103005.jpg",
     rating: 8.75,
     genres: "Action, Adventure, Drama",
-    aired: "Jul 8, 2019 to Dec 30, 2019"
+    aired: "Jul 8, 2019 to Dec 30, 2019",
+    wiki: "https://en.wikipedia.org/wiki/Vinland_Saga_(manga)"
 },
 {
     id: 11,
@@ -84,7 +95,8 @@ let komikList = [{
     imgCover: "https://cdn.myanimelist.net/images/anime/8/80356.jpg",
     rating: 8.49,
     genres: "Action, Comedy, Supernatural",
-    aired: "Jul 11, 2016 to Sep 27, 2016"
+    aired: "Jul 11, 2016 to Sep 27, 2016",
+    wiki: "https://en.wikipedia.org/wiki/Mob_Psycho_100"
 },
 {
     id: 12,
@@ -92,7 +104,8 @@ let komikList = [{
     imgCover: "https://cdn.myanimelist.net/images/anime/7/76014.jpg",
     rating: 8.44,
     genres: "Sports",
-    aired: "Apr 6, 2014 to Sep 21, 2014"
+    aired: "Apr 6, 2014 to Sep 21, 2014",
+    wiki: "https://en.wikipedia.org/wiki/Haikyuu!!"
 },
 {
     id: 13,
@@ -100,7 +113,8 @@ let komikList = [{
     imgCover: "https://cdn.myanimelist.net/images/anime/12/76049.jpg",
     rating: 8.5,
     genres: "Action, Comedy",
-    aired: "Oct 5, 2015 to Dec 21, 2015"
+    aired: "Oct 5, 2015 to Dec 21, 2015",
+    wiki: "https://en.wikipedia.org/wiki/One_Punch_Man"
 },
 {
     id: 14,
@@ -108,7 +122,8 @@ let komikList = [{
     imgCover: "https://cdn.myanimelist.net/images/anime/1830/118780.jpg",
     rating: 8.5,
     genres: "Fantasy, Mystery, Sci-Fi, Suspense",
-    aired: "Jan 10, 2019 to Mar 29, 2019"
+    aired: "Jan 10, 2019 to Mar 29, 2019",
+    wiki: "https://id.wikipedia.org/wiki/The_Promised_Neverland"
 },
 {
     id: 15,
@@ -117,6 +132,7 @@ let komikList = [{
     rating: 7.79,
     genres: "Action, Fantasy, Horror, Suspense",
     aired: "Oct 3, 2002 to Feb 8, 2007",
-    
-},
+    wiki: "https://en.wikipedia.org/wiki/Tokyo_Ghoul"
+
+}
 ]
