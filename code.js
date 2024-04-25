cd64fabb8f892ee7825b4ec5b52427253622d2e8
welferdinand <welferdinand@gmail.com>
2024-04-25 20:29:36 +0700

    sixth commit

--- code.js
+++ code.js
@@ -1,3 +1,5 @@
+let arrayYangSudahBerubah = null;
+
 function render(array) {
   let listKomik = document.getElementById("render");
 
@@ -8,21 +10,21 @@ function render(array) {
 
     listKomik.innerHTML += `<div class="card rounded" style="width: 18rem;">
     <div class="img rounded">
-    <img src="${imgCover}" class="card-img-top" alt="...">
+    <img id="coverImage" src="${imgCover}" class="card-img-top" alt="...">
     </div>
     <div class="card-body ">
       <div class="judul">
       <h3>${judul}</h3>
       </div>
       <div class="desciption">
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    </div>
+      <p class="card-text">Genre: ${genres}</p>
+      </div>
     <div class="d-flex justify-content-evenly jarak">
-      <div class="text-center ">
-      <a href="#" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#exampleModal"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
-        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/>
-        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
-      </svg></a>
+    <div class="text-center " onclick="editAnime(${id})">
+    <a href="#" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#exampleModal"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16" >
+    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+    <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+    </svg>  </a>
     </div>
       <div class="text-center ">
       <a href="#" class="btn btn-outline-warning"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
@@ -46,10 +48,12 @@ function render(array) {
     let button = deleteButtons[i];
     button.addEventListener("click", function () {
       let id = this.getAttribute("data-id");
-      deleteKomik(id);  
+      deleteKomik(id);
     });
   }
 }
+
+render(komikList);
 
 function deleteKomik(id) {
   console.log("Menghapus komik dengan ID:", id);
@@ -64,8 +68,109 @@ function deleteKomik(id) {
   render(komikList);
 }
 
-function tambahKomik(id){
-  
+let createButtons = document.querySelectorAll(".add");
+for (let i = 0; i < createButtons.length; i++) {
+  let button = createButtons[i];
+  button.addEventListener("click", function () {
+    let id = this.getAttribute("data-id");
+    tambahKomik(id);
+  });
 }
 
-render(komikList);
+function editAnime(id) {
+  console.log(id);
+  localStorage.setItem("idEdit", String(id));
+
+  let komikYangDiEdit = {};
+
+  if (arrayYangSudahBerubah === null) {
+    for (let i = 0; i < komikList.length; i++) {
+      let perKomik = komikList[i];
+
+      let idKomik = perKomik.id;
+
+      if (id === idKomik) {
+        komikYangDiEdit = { ...perKomik };
+      }
+    }
+  } else {
+    for (let i = 0; i < arrayYangSudahBerubah.length; i++) {
+      let perKomik = arrayYangSudahBerubah[i];
+
+      let idKomik = perKomik.id;
+
+      if (id === idKomik) {
+        komikYangDiEdit = { ...perKomik };
+      }
+    }
+  }
+
+  let inputJudul = document.getElementById("recipient-name");
+  inputJudul.value = komikYangDiEdit.judul;
+  let inputDeskrip = document.getElementById("message-text");
+  inputDeskrip.value = komikYangDiEdit.genres;
+  let inputImg = document.getElementById("coverImage");
+  // inputImg = komikYangDiEdit.imgCover;
+}
+
+function simpanEdit() {
+  let idNow = Number(localStorage.getItem("idEdit"));
+  let inputImg = document.getElementById("coverImage");
+  // inputImg = komikYangDiEdit.imgCover;
+
+  let inputJudulValue = document.getElementById("recipient-name").value;
+  let desciption = document.getElementById("message-text").value;
+  let gambar = document.getElementById("basic-url").value;
+  console.log(gambar);
+
+  let newArray = [];
+  let komikYangDiEdit = {};
+
+  if (arrayYangSudahBerubah === null) {
+    for (let i = 0; i < komikList.length; i++) {
+      let perKomik = komikList[i];
+
+      let idKomik = perKomik.id;
+
+      if (idNow !== idKomik) {
+        newArray.push(perKomik);
+      } else {
+        komikYangDiEdit = {
+          id: perKomik.id,
+          judul: inputJudulValue,
+          imgCover: gambar,
+          rating: perKomik.rating,
+          genres: desciption,
+          aired: perKomik.aired,
+        };
+
+        newArray.push(komikYangDiEdit);
+      }
+    }
+  } else {
+    for (let i = 0; i < arrayYangSudahBerubah.length; i++) {
+      let perKomik = arrayYangSudahBerubah[i];
+
+      let idKomik = perKomik.id;
+
+      if (idNow !== idKomik) {
+        newArray.push(perKomik);
+      } else {
+        komikYangDiEdit = {
+          id: perKomik.id,
+          judul: inputJudulValue,
+          imgCover: gambar,
+          rating: perKomik.rating,
+          genres: desciption,
+          aired: perKomik.aired,
+        };
+
+        newArray.push(komikYangDiEdit);
+      }
+    }
+  }
+
+  arrayYangSudahBerubah = [...newArray];
+
+  render(newArray);
+}
