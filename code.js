25c7f144827554dad5ed27e1e487c6e0922a6a9d
welferdinand <welferdinand@gmail.com>
2024-04-25 15:44:53 +0700

    fourth commit

--- code.js
+++ code.js
@@ -1,26 +1,71 @@
-function render(array){
-    let cetak = document.getElementById("render")
-    
-    cetak.innerHTML = ""
-   
-    for(let i = 0; i < array.length; i++){
-       let {id, judul, imgCover, rating, genres, aired} = array[i]
-   
-       cetak.innerHTML += `<div class="card rounded" style="width: 18rem;">
-       <div class="img rounded">
-       <img src="${imgCover}" class="card-img-top" alt="...">
-       </div>
-       <div class="card-body">
-         <h3>${judul}</h3>
-         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-         <div class="text-center">
-         <a href="#" class="btn d-flex justify-content-center">Read</a>
-       </div>
-       </div>
-     </div>`
-   
+function render(array) {
+  let listKomik = document.getElementById("render");
+
+  listKomik.innerHTML = "";
+
+  for (let i = 0; i < array.length; i++) {
+    let { id, judul, imgCover, rating, genres, aired } = array[i];
+
+    listKomik.innerHTML += `<div class="card rounded" style="width: 18rem;">
+    <div class="img rounded">
+    <img src="${imgCover}" class="card-img-top" alt="...">
+    </div>
+    <div class="card-body ">
+      <div class="judul">
+      <h3>${judul}</h3>
+      </div>
+      <div class="desciption">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+    <div class="d-flex justify-content-evenly jarak">
+      <div class="text-center ">
+      <a href="#" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#exampleModal"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/>
+        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
+      </svg></a>
+    </div>
+      <div class="text-center ">
+      <a href="#" class="btn btn-outline-warning"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+      <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+      <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+      </svg></a>
+      </div>
+      <div class="text-center ">
+      <a href="#" class="btn btn-outline-danger delete" data-id="${id}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+      <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+        </svg></a>
+    </div>
+    </div>
+  </div>
+  </div>`;
+  }
+
+  // Menambahkan event listener untuk setiap tombol delete
+  let deleteButtons = document.querySelectorAll(".delete");
+  for (let i = 0; i < deleteButtons.length; i++) {
+    let button = deleteButtons[i];
+    button.addEventListener("click", function () {
+      let id = this.getAttribute("data-id");
+      deleteKomik(id);  
+    });
+  }
+}
+
+function deleteKomik(id) {
+  console.log("Menghapus komik dengan ID:", id);
+  let temp = [];
+
+  for (let i = 0; i < komikList.length; i++) {
+    if (komikList[i].id !== Number(id)) {
+      temp.push(komikList[i]);
     }
-   }
-   
-   render(komikList)
-   
+  }
+  komikList = temp;
+  render(komikList);
+}
+
+function tambahKomik(id){
+  
+}
+
+render(komikList);
